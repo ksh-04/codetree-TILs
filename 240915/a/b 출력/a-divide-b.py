939d3af6e97e5bbe1556588528e780123785348b
ksh-04 <ksh04@ewha.ac.kr>
2024-09-15 19:55:14 +0900

    Title: a/b 출력, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240915/a/b 출력/a-divide-b.py
+++ 240915/a/b 출력/a-divide-b.py
@@ -1,7 +1,9 @@
 a, b = map(int, input().split())
 
 print(a // b, ".", sep = "", end = "")
+
 for _ in range(20):
+    a = a - b * (a // b)
     a = a * 10
     print(a // b, end = "")
     a = a - b * (a // b)
