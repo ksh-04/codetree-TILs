0821e2cc40b92ba9e56f368f53af8173587d2dee
ksh-04 <ksh04@ewha.ac.kr>
2024-09-17 22:10:24 +0900

    Title: 출석 부르기 2, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240917/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,18 @@
+while True:
+    n = int(input())
+
+    if n == 1:
+        print("John")
+        continue
+    elif n == 2:
+        print("Tom")
+        continue
+    elif n == 3:
+        print("Paul")
+        continue
+    elif n == 4:
+        print("Sam")
+        continue
+    else:
+        print("Vacancy")
+        break
