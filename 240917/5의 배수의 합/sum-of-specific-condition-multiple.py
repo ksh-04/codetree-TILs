82d93521a51552894c2f66983cf0287c44259fc4
ksh-04 <ksh04@ewha.ac.kr>
2024-09-17 20:51:55 +0900

    Title: 5의 배수의 합, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240917/5의 배수의 합/sum-of-specific-condition-multiple.py
@@ -0,0 +1,14 @@
+sum = 0
+
+a, b = map(int, input().split())
+
+if a < b:
+    for i in range(a, b+1):
+        if i % 5 == 0:
+            sum += i
+else:
+    for i in range(b, a+1):
+        if i % 5 == 0:
+            sum += i
+
+print(sum)
